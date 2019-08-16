920271bcfd56cf2238c044ef444aa671697795ca
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-08-16 16:54:24 -0700

    copy updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ function getIcon(name) {
   }
 }
 
-function ExternalLink({ href, target = "_blank", children }) {
+function Ahref({ href, target = "_blank", children }) {
   return (
     <a
       target={target}
@@ -98,34 +98,29 @@ const IndexPage = ({ data }) => {
       <br />
       <Section title="Software & Startups">
         <p className="pb-3">
-          I've been an entreprenuer ever since I started my first business
-          selling logos and websites on eBay as a 12 year old. As you could
-          imagine, phone calls didn't go as planned.
+          I always knew I wanted to build things. At the age of 12 I started my
+          first business selling templated logos and websites on Ebay. As you
+          could imagine, client phone calls didn't go as planned.
         </p>
         <p className="pb-3">
-          I've started several companies and have learned a lot along the way.
-          Right now I'm working on a product helping teams build apps visually
-          called{" "}
-          <ExternalLink href="https://draftbit.com">Draftbit</ExternalLink>, a
-          <ExternalLink href="https://ycombinator.com">
-            Y-Combinator backed
-          </ExternalLink>{" "}
-          company.
+          Several companies and *lots* of learning, experiences and mistakes
+          later, I'm working on a new way to build apps.{" "}
         </p>
         <p className="pb-3">
-          I'm a highly energetic and driven leader, entrepreneur and speaker. I
-          pride myself on building companies by finding an edge in software.
+          <Ahref href="https://draftbit.com">Draftbit</Ahref> is a platform
+          backed by <Ahref href="https://ycombinator.com">Y-Combinator</Ahref>{" "}
+          that gives teams the ability to build fully native experiences without
+          having to worry about writing any code, giving anyone the opportunity
+          to build anything they imagine.
         </p>
         <p>
-          Some of the software I've been obsessed with recently:
+          Some of the software and frameworks I've been obsessed with recently:
           <ul className="mt-2">
             <li className="mb-1">
-              <ExternalLink href="https://reasonml.github.io/">
-                ReasonML
-              </ExternalLink>
+              <Ahref href="https://reasonml.github.io/">ReasonML</Ahref>
             </li>
             <li className="mb-1">
-              <ExternalLink href="https://expo.io">Expo</ExternalLink>
+              <Ahref href="https://expo.io">Expo</Ahref>
             </li>
             <li className="mb-1">React Native</li>
             <li className="mb-1">React</li>
@@ -135,30 +130,25 @@ const IndexPage = ({ data }) => {
       </Section>
       <Section title="Community">
         <p>
-          I wouldn't be able to get to where I am today if it wasn't for the
-          folks that believed in me. I like to pay it forward by donating time &
-          energy to building a real tech community in Chicago:
+          People and relationships are what made me the person I am today,
+          building a rich and striving tech community in Chicago is a
+          vision/passion of mine. These are some of the awesome communities that
+          are making my vision come true
         </p>
         <ul className="mt-2">
           <li className="mb-1">
-            <ExternalLink href="https://chicagojscamp.org">
-              Chicago JSCamp
-            </ExternalLink>
+            <Ahref href="https://chicagojscamp.org">Chicago JSCamp</Ahref>
           </li>
           <li className="mb-1">
-            <ExternalLink href="https://chicagojs.org">
-              ChicagoJS Community
-            </ExternalLink>
+            <Ahref href="https://chicagojs.org">ChicagoJS Community</Ahref>
           </li>
           <li className="mb-1">
-            <ExternalLink href="https://reason-conf.us">
-              Reason Conf US
-            </ExternalLink>
+            <Ahref href="https://reason-conf.us">Reason Conf US</Ahref>
           </li>
           <li className="mb-1">
-            <ExternalLink href="http://www.chicagotechslack.com">
+            <Ahref href="http://www.chicagotechslack.com">
               Chicago Tech Slack
-            </ExternalLink>
+            </Ahref>
           </li>
         </ul>
       </Section>
@@ -172,19 +162,19 @@ const IndexPage = ({ data }) => {
       <Section title="Get In Touch">
         <p>
           The best way to contact me is through{" "}
-          <ExternalLink
+          <Ahref
             className="border-b-2 border-gray-600 text-white"
             href="https://twitter.com/peterpme"
           >
             Twitter
-          </ExternalLink>{" "}
+          </Ahref>{" "}
           or by sending me an{" "}
-          <ExternalLink
+          <Ahref
             className="border-b-2 border-gray-600 text-white"
             href="mailto:peter@peterp.me"
           >
             e-mail
-          </ExternalLink>
+          </Ahref>
           . I look forward to hearing from you.
         </p>
       </Section>
